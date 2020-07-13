e4cc19a036faea451a3ea62aa5e063dad9f8246a
EmorZz1G <1227117695@qq.com>
2020-07-13 19:11:13 +0800

    add GPL comment

--- src/main/java/com/finance/controller/TestController.java
+++ src/main/java/com/finance/controller/TestController.java
@@ -1,12 +1,8 @@
 package com.finance.controller;
 
-import com.finance.mapper.others.BankMapper;
 import com.finance.mapper.others.BankcardMapper;
 import com.finance.mapper.user.UserMapper;
 import com.finance.pojo.others.Bankcard;
-import com.finance.pojo.user.User;
-import com.finance.pojo.user.UserExample;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +12,9 @@ import java.util.List;
 
 @Controller
 public class TestController {
+
+
+    // 我写了一个GPL
 
     @Resource
     BankcardMapper bankcardMapper;
